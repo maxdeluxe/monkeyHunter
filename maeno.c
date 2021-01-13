a4d31052f08bec7e0f8a4324f7e484dbad0cfc34
Houjitya0 <62522046+Houjitya0@users.noreply.github.com>
2021-01-13 17:12:33 +0900

    third

--- maeno.c
+++ maeno.c
@@ -39,25 +39,28 @@ int main(void)
     MASS ball[N];//弾丸のデータ
 
     //弾丸の初期値
-    double ball_v0 = 40;  //初速度
+    double ball_v0 = 21;  //初速度 計算値20.6
     double ball_vx0, ball_vy0; //初速度x,y成分
     double ball_y0 = 0;      //y座標
     double ball_x0 = 0;      //x座標
-    double ball_angle = 10;   //弾丸の仰角
+
+    double ball_angle = 45;   //弾丸の仰角
+
+
+    // double ball_angle = 90 * M_PI / 180;   //弾丸の仰角
     //の初期値
-    double monkey_v0 = 0.0; //サルの初速度
+    double monkey_v0 =  0.0; //サルの初速度
     double monkey_vy0 = 0.0;
     double monkey_vx0 = 0.0;
     double monkey_y0 = 40;      //猿の初期位置（垂直方向)
     double monkey_x0 = 40;      //猿の初期位置（水平方向）
-
     double t = 0.0;
     double ts = 0.005;      //時間分割
     double gravity = 9.8;       //重力加速度
     double ref = -0.4;//反発係数
 
-    ball_vx0 = ball_v0 * cos(ball_angle*M_PI/180.0);
-    ball_vy0 = ball_v0 * sin(ball_angle*M_PI/180.0);
+    ball_vx0 = ball_v0 * cos(ball_angle* M_PI/ 180.0);
+    ball_vy0 = ball_v0 * sin(ball_angle* M_PI/ 180.0);
 
     ball[0].v0 = ball_v0;
     ball[0].y = ball[0].y0 = ball_y0;
@@ -77,16 +80,15 @@ int main(void)
     monkey[0].pt = 0.0;
 
     calc(ball, monkey, gravity, ref, ts, N, &total);
-    graph_draw2(ball, monkey, total, req);//アニメーション関数１<-使う場合なgraph_draw2,3をコメント文にする
-    //graph_draw2(ball, monkey, total, req);//アニメーション関数２<-使う場合はgraph_draw1,3をコメント文にする
-    //graph_draw3(ball, monkey, total, req);//アニメーション関数３<-使う場合はgraph_draw1,2をコメント文にする
+    //graph_draw1(ball, monkey, total, req);
+    //graph_draw2(ball, monkey, total, req);
+    graph_draw3(ball, monkey, total, req);
 
     printf("データをファイルに書き込み保存\n");
     data_write(ball, monkey, total);
 
     return 0;
 }
-
 
 //モンキーハンティングのシミュレーション関数
 //引数１：弾丸
@@ -98,59 +100,73 @@ int main(void)
 //引数７：計算回数を返すためのポインタ変数
 void calc(MASS b[], MASS m[], double g, double r, double ts, int n, int *total)
 {
-int monkey_bound = 0;
-int ball_bound = 0;
-int count=0;
-int flg1 = 0;
-int i = 1;
-double t = 0.0;
-do{
-b[i].x = b[i-1].x + b[i-1].vx * ts;   //弾丸のx座標の計算
-b[i].y = b[i-1].y + b[i-1].vy * ts;   //弾丸のy座標の計算
-b[i].vx = b[i-1].vx;                  //弾丸のx方向速度の計算
-b[i].vy = b[i-1].vy - g * ts;         //弾丸のy方向速度の計算
-m[i].x = m[i-1].x + m[i-1].vx * ts;   //サルのx座標の計算
-m[i].vx = m[i-1].vx;                  //サルのx方向速度の計算
-m[i].y = m[i-1].y + m[i-1].vy * ts;   //サルのy方向位置の計算
-m[i].vy = m[i-1].vy - g * ts;         //サルのy方向速度の計算
-double diff_x = b[i].x - m[i].x;//弾丸とサルの水平位置の差
-double diff_y = b[i].y - m[i].y;//弾丸とサルの垂直位置の差
-if(diff_x < 0.0) diff_x *= -1.0;//差の絶対値
-if(diff_y < 0.0) diff_y *= -1.0;//差の絶対値
-//弾丸とサルとの衝突判定うまく判定できない場合にはdiff_xとdiff_yの比較条件（値）を変更すること
-if(count > 10 && diff_x < 0.5 && diff_y < 0.5){
-b[i].vx = b[i].vx * r + m[i].vx * r;
-m[i].vx = m[i].vx * r + b[i].vx * r;
-b[i].vy = b[i].vy * r + m[i].vy * r;
-m[i].vy = m[i].vy * r + b[i].vy * r;
-}
-//弾丸と地面との衝突判定
-if(b[i].y <= 0.0 && b[i].vy < 0.0){
-b[i].vy  *= r;
-b[i].vx -= b[i].v0/1000.0;//横方向の地面との摩擦を適当に
-ball_bound++;
-}
-//サルと地面との衝突判定
-if(m[i].y <= 0 && m[i].vy < 0){
-m[i].vy *= r;
-m[i].vx -= m[i-1].vx/1000;//横方向の地面との摩擦を適当に
-monkey_bound++;
-}
-b[i].pt = t;
-m[i].pt = t;
-t += ts;
-count++;
-i++;
-if(t > 0.0 )flg1 = 1;
-else if( b[i].y > -0.1 ) flg1 = 1;
-else flg1 = 0;
-if(monkey_bound > 5)flg1 = 0;//サルが4回以上地面に落下した場合
-if(ball_bound > 2) flg1 = 0;//弾丸が1回以上地面に落下した場合
-else if( count > n - 1) flg1 = 0;//用意した配列の要素数以上になった場合
-}while(flg1 == 1);
-*total = i - 1;//計算した値の個数
-}
+    int monkey_bound = 0;
+    int ball_bound = 0;
+    int count=0;
+    int flg1 = 0;
+    int i = 1;
+    double t = 0.0;
 
+    do{
+        b[i].y = b[i-1].y + b[i-1].vy * ts;   //弾丸のy座標の計算
+
+        b[i].x = b[i-1].x + b[i-1].vx * ts;   //弾丸のx座標の計算
+
+
+        // b[i].vy = b[i-1].y - g * ts;         //弾丸のy方向速度の計算
+        b[i].vy = b[i-1].vy - g * ts;         //弾丸のy方向速度の計算
+
+        b[i].vx = b[i-1].vx;                  //弾丸のx方向速度の計算
+
+
+        m[i].vx = m[i-1].vx;                  //サルのx方向速度の計算
+        m[i].x = m[i-1].x + m[i-1].vx * ts;   //サルのx座標の計算
+
+
+        m[i].vy = m[i-1].vy - g * ts;         //サルのy方向速度の計算
+        //m[i].y =  m[i-1].y - g*ts;              //サルのy方向位置の計算
+        m[i].y = m[i-1].y + m[i].vy * ts;
+
+        double diff_x = b[i].x - m[i].x;//弾丸とサルの水平位置の差
+        double diff_y = b[i].y - m[i].y;//弾丸とサルの垂直位置の差
+        if(diff_x < 0.0) diff_x *= -1.0;//差の絶対値
+        if(diff_y < 0.0) diff_y *= -1.0;//差の絶対値
+
+        //弾丸とサルとの衝突判定うまく判定できない場合にはdiff_xとdiff_yの比較条件（値）を変更すること
+        if(count > 10 && diff_x < 0.5 && diff_y < 0.5){
+            b[i].vx = b[i].vx * r + m[i].vx * r;
+            m[i].vx = m[i].vx * r + b[i].vx * r;
+            b[i].vy = b[i].vy * r + m[i].vy * r;
+            m[i].vy = m[i].vy * r + b[i].vy * r;
+        }
+        //弾丸と地面との衝突判定
+        if(b[i].y <= 0.0 && b[i].vy < 0.0){
+            b[i].vy  *= r;
+            b[i].vx -= b[i].v0/1000.0;//横方向の地面との摩擦を適当に
+            ball_bound++;
+        }
+
+        //サルと地面との衝突判定
+        if(m[i].y <= 0 && m[i].vy < 0){
+            m[i].vy *= r;
+            m[i].vx -= m[i-1].vx/1000;//横方向の地面との摩擦を適当に
+            monkey_bound++;
+        }
+        b[i].pt = t;
+        m[i].pt = t;
+
+        t += ts;
+        count++;
+        i++;
+        if(t > 0.0 )flg1 = 1;
+        else if( b[i].y > -0.1 ) flg1 = 1;
+        else flg1 = 0;
+        if(monkey_bound > 5)flg1 = 0;//サルが4回以上地面に落下した場合
+        if(ball_bound > 2) flg1 = 0;//弾丸が1回以上地面に落下した場合
+        else if( count > n - 1) flg1 = 0;//用意した配列の要素数以上になった場合
+    }while(flg1 == 1);
+    *total = i - 1;//計算した値の個数
+}
 
 //計算結果をグラフ表示する関数１
 //引数１：弾丸
@@ -317,10 +333,6 @@ void graph_draw3(MASS b[], MASS m[], int n, struct timespec req)
     pclose(gp);
 }
 
-//計算結果をファイルに書き込む関数
-//引数１：弾丸
-//引数２：サル
-//引数３：データ数
 //計算結果をファイルに書き込む関数
 //引数１：弾丸
 //引数２：サル
